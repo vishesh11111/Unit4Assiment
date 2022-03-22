7d5f782f86464381926b52a4d148a5acf0a9d447
Vishesh Kushwaha <82991580+vishesh11111@users.noreply.github.com>
2022-03-22 14:58:52 +0530

    Update index.js

--- day_13Assigment/src/index.js
+++ day_13Assigment/src/index.js
@@ -1,7 +1,6 @@
 
 const express = require("express");
-
-// const { body, validationResult } = require('express-validator');
+const { body } = require('express-validator');
 const {register, login} = require("./controllers/auth.controller");
 const Usercontroller = require("./controllers/user.controller");
 const productController = require("./controllers/userProduct.controller");
@@ -13,6 +12,10 @@ router.use("/users", Usercontroller);
 
 router.post("/login", login);
 
-router.post("/register",register);
+router.post("/register",
+            body("email").not().isEmpty()
+.isEmail().withMessage("Enter valid Email"),
+body("password").not().isEmpty().isLength({min: 8}, {max: 10})
+,register);
 router.use("/products", productController);
 module.exports = router;
